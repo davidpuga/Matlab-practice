56501c5b486f841f72ff0b17096fe5c6f7989e7a
davidpuga <david.puga@embl.de>
2016-08-15 19:04:24 +0200

    added week2 assignment

---
+++ sum.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import re
+
+name = raw_input("Enter file:")
+if len(name) < 1 : name = "regex_sum_305519.txt" 
+handle = open(name)
+num = list()
+for line in handle:
+	num_str = re.findall("\d+", line)
+	if len(num_str) <= 0:continue 
+	for strings in num_str:
+		number = int(strings)
+		num.append(number)
+total = sum(num)
+print total
